0f17470f1120c58b0b988db138d58f44ebb1b6fa
BBJM <BIBOJM@localhost>
2016-09-08 23:03:22 +0800

    Initial Commit

---
+++ YJMMakeYouBeauty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJMMakeYouBeauty
+//
+//  Created by BBJM on 16/9/8.
+//  Copyright © 2016年 YQJM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YJMMakeYouBeauty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJMMakeYouBeauty
+//
+//  Created by BBJM on 16/9/8.
+//  Copyright © 2016年 YQJM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
